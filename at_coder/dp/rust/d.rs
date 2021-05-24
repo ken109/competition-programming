b7a841684dd0b7c4fc07e9a7a6be8f809aa5f0b0
Kensuke Kubo <kensukekubo19@gmail.com>
2021-05-25 01:15:16 +0900

    refactor

--- at_coder/dp/rust/d.rs
+++ at_coder/dp/rust/d.rs
@@ -1,5 +1,4 @@
 use proconio::input;
-use std::cmp::max;
 
 fn main() {
     input! {
@@ -7,12 +6,12 @@ fn main() {
         wv: [(usize, usize); n]
     }
 
-    let mut dp = vec![vec![0usize; w + 1]; n + 1];
+    let mut dp = vec![vec![0 as usize; w + 1]; n + 1];
 
     for (i, (weight, value)) in wv.iter().enumerate() {
         for w in 0..=w {
             if w >= *weight {
-                dp[i + 1][w] = max(dp[i][w], dp[i][w - *weight] + *value);
+                dp[i + 1][w] = dp[i][w].max(dp[i][w - *weight] + *value);
             } else {
                 dp[i + 1][w] = dp[i][w];
             }
--- at_coder/zone2021/rust/a.rs
+++ at_coder/zone2021/rust/a.rs
@@ -1,12 +1,9 @@
 use proconio::input;
-use proconio::marker::Chars;
 
 fn main() {
     input! {
-        cs: Chars
+        s: String
     }
-
-    let s: String = cs.iter().collect();
 
     println!("{}", s.matches("ZONe").count())
 }
